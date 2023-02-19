a3429ed51f42caf025d460a4eb7201b3040b3b95
Geoff Rich <4992896+geoffrich@users.noreply.github.com>
2023-02-18 19:08:47 -0800

    Restore scroll on /summit pages

--- src/routes/summit-[year]/+page.svelte
+++ src/routes/summit-[year]/+page.svelte
@@ -9,13 +9,31 @@
 	$: filteredVideos = Object.fromEntries(
 		Object.entries(videos).filter(([_, data]) => data.category === selectedYear)
 	);
+
+	/** @type { HTMLElement}  */
+	let container;
+
+	/** @type {import('./$types').Snapshot} */
+	export const snapshot = {
+		capture: () => {
+			// we need to manually restore the scroll of the parent element
+			// SvelteKit won't, because it's not the window scroll
+			// TODO: is this better in an afterNavigate/afterTransition?
+			return container.parentElement?.scrollTop;
+		},
+		restore: (scrollTop) => {
+			if (container.parentElement) {
+				container.parentElement.scrollTop = scrollTop;
+			}
+		}
+	};
 </script>
 
 <svelte:head>
 	<title>Svelte Summit - {selectedYear}</title>
 </svelte:head>
 
-<div>
+<div bind:this={container}>
 	<Switch {selectedYear} />
 	<VideoList videos={filteredVideos} />
 </div>
